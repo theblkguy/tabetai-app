5c4b1fabeece1881e6a0147325f9d3c04483b569
theblkguy <allblk13@gmail.com>
2025-07-04 16:23:22 -0500

    Add/ Babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'client', 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
