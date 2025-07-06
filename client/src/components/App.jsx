79ab68a27254bdb14b9769c9364b949cde5b57a0
JasmineHall1992 <jasminehall5935@gmail.com>
2025-07-05 20:00:21 -0500

    added react-router dependency, made new components(homepage,profilepage,recipepage) to go in app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
+//make a homepage component
+import HomePage from ''
+//make a recipe page component
+//perhaps make a profile page component
 
 class App extends React.Component {
   constructor(props){
     super(props);
   }
 
-
+//render the different routes to the page
 render() {
   return (
     <div>
-      <h1>Good Morning, World!</h1>
+      <Route path="/" element={<HomePage />} />
     </div>
   )
 }
