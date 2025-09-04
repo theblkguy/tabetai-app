62a3e137ff7f839694e7063f7c8dfc0cb13b93a5
theblkguy <allblk13@gmail.com>
2025-09-03 23:19:29 -0500

    Restore full React app and API functionality on port 80

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //5. Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
